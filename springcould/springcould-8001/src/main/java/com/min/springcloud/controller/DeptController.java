4ed370b122f18cc12b33bce72c17be8efc65489d
loveGetHub <1063084728@qq.com>
2020-08-19 16:05:11 +0800

    eureka集群配置

--- springcould/springcould-8001/src/main/java/com/min/springcloud/controller/DeptController.java
+++ springcould/springcould-8001/src/main/java/com/min/springcloud/controller/DeptController.java
@@ -4,6 +4,7 @@ import com.min.springcloud.dao.DeptDao;
 import com.min.springcloud.service.DeptService;
 import entities.Dept;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,6 +13,8 @@ import java.util.List;
 public class DeptController {
     @Autowired
     private DeptService deptService;
+    @Autowired
+    private DiscoveryClient discoveryClient;
     @RequestMapping(value = "/dept/add" ,method = RequestMethod.POST)
     public boolean add(@RequestBody Dept dept){
         return deptService.add(dept);
@@ -23,5 +26,10 @@ public class DeptController {
     @RequestMapping(value = "/dept/list" ,method = RequestMethod.GET)
     public List<Dept> list(){
         return deptService.list();
+    }
+    @RequestMapping(value = "dept/discovery",method = RequestMethod.GET)
+    public Object discovery(){
+        List<String> list = discoveryClient.getServices();
+        return list;
     }
 }
